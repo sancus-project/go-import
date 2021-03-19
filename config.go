7194dd9830d2e1f66ee21eb9d1139887fa250a6e
Alejandro Mery <amery@geeks.cl>
2021-03-19 11:44:56 +0000

    migrate to using go.sancus.dev/middleware/goget
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- config.go
+++ config.go
@@ -2,25 +2,26 @@ package app
 
 import (
 	"gopkg.in/gcfg.v1"
+
+	"go.sancus.dev/middleware/goget"
 )
 
 // config.ini
-type Package struct {
-	VCS  string
-	URL string
-}
-
 type Config struct {
 	HTTP struct {
 		Address string
 	}
-	Package map[string]*Package
+	Package goget.Packages
 }
 
 func ConfigFromFile(fn string) (*Config, error) {
 	var ini Config
 
 	if err := gcfg.ReadFileInto(&ini, fn); err != nil {
+		return nil, err
+	}
+
+	if err := ini.Package.SetDefaults(); err != nil {
 		return nil, err
 	}
 
--- server.go
+++ server.go
@@ -1,66 +1,50 @@
 package app
 
 import (
-	"fmt"
-	"go.sancus.dev/sancus/attic/log"
-	"go.sancus.dev/sancus/web"
 	"net/http"
 	"strings"
+
+	"go.sancus.dev/middleware/goget"
+	"go.sancus.dev/sancus/attic/log"
+	"go.sancus.dev/sancus/web"
 )
 
 // handler
 type handler struct {
-	packages map[string]*Package
+	renderer goget.Renderer
+	packages goget.Packages
 	logger   *log.Logger
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	url := r.Host + r.URL.Path
 
-	for k, v := range h.packages {
+	if v := h.packages.Get(r); v != nil {
+		k := v.Canonical
 		s := strings.TrimPrefix(url, k)
-		if s == "" || s[0] == '/' {
-			if s == "" {
-				h.logger.Info("%s -> %s+%s", k, v.VCS, v.URL)
-			} else {
-				h.logger.Info("%s (%s) -> %s+%s", k, s, v.VCS, v.URL)
-			}
 
-			s = "https://godoc.org/" + url
-
-			fmt.Fprintf(w, "<!DOCTYPE html>\n<head>\n")
-			fmt.Fprintf(w, "\t<meta name=\"go-import\" content=\"%v %v %v\">\n", k, v.VCS, v.URL)
-			fmt.Fprintf(w, "\t<meta http-equiv=\"refresh\" content=\"5; url=%v\" />\n", s)
-
-			fmt.Fprintf(w, "</head>\n<body>\n")
-			fmt.Fprintf(w, "<pre>git clone <a href=\"%v\">%v</a>\n</pre>", v.URL, v.URL)
-			fmt.Fprintf(w, "<pre>go get <a href=\"%v\">%v</a></pre>\n", s, url)
-			fmt.Fprintf(w, "<pre>import \"<a href=\"%v\">%v</a>\"</pre>\n", s, url)
-			fmt.Fprintf(w, "</body>\n")
-			return
+		if s == "" {
+			h.logger.Info("%s -> %s+%s", k, v.VCS, v.Repository)
+		} else {
+			h.logger.Info("%s (%s) -> %s+%s", k, s, v.VCS, v.Repository)
 		}
-	}
 
-	h.logger.Warn("%v: not recognised", url)
-	http.NotFound(w, r)
-	return
+		if err := h.renderer(w, r, v); err != nil {
+			h.logger.Fatal("%v: %s", url, err)
+		}
+	} else {
+
+		h.logger.Warn("%v: not recognised", url)
+		http.NotFound(w, r)
+	}
 }
 
-func NewHandler(packages map[string]*Package, l *log.Logger) http.Handler {
+func NewHandler(packages goget.Packages, l *log.Logger) http.Handler {
 	h := handler{
-		packages: make(map[string]*Package),
+		packages: packages,
+		renderer: goget.DefaultRenderer(),
 		logger:   l.SubLogger(":dispatcher"),
-	}
-
-	for k, v := range packages {
-		if v.URL == "" {
-			continue
-		}
-		if v.VCS == "" {
-			v.VCS = "git"
-		}
-
-		h.packages[k] = v
 	}
 
 	return &h
